47ea158eb80de5cc188f6b3b4d6dd1126b26d1ba
Onur CEVIK <onurcevik61@hotmail.com>
2021-03-22 12:46:05 +0300

    tests updated for coverage

--- database/mysql_test.go
+++ database/mysql_test.go
@@ -6,27 +6,46 @@ import (
 
 func TestMySQL_Check(t *testing.T) {
 	var d Dump
-	d=MySQL{}
-	err:=d.Check()
+	d = MySQL{}
+	d.Check()
+}
+
+func TestMySQL_CheckWithError(t *testing.T) {
+	var d Dump
+	d = MySQL{}
+	err := d.Check()
 	if err != nil {
-		t.Errorf("MySQL Check() Error: %s",err.Error())
+		t.Errorf("MySQL Check() Error: %s", err.Error())
 	}
 }
 
 func TestMySQL_Import(t *testing.T) {
 	var d Dump
-	d=MySQL{}
-	err:=d.Import("testuser","/home/onur123/go/src/github.com/onurcevik/DummyDump/1616151265428723615.backup.sql")
+	d = MySQL{}
+	d.Import("testuser", "/home/onur123/go/src/github.com/onurcevik/DummyDump/test.sql")
+}
+
+func TestMySQL_ImportWithError(t *testing.T) {
+	var d Dump
+	d = MySQL{}
+	err := d.Import("testuser", "/home/onur123/go/src/github.com/onurcevik/DummyDump/test.sql")
 	if err != nil {
-		t.Errorf("MySQL Import() Error: %s",err.Error())
+		t.Errorf("MySQL Import() Error: %s", err.Error())
 	}
 }
 
 func TestMySQL_Export(t *testing.T) {
 	var d Dump
-	d=MySQL{}
-	err:=d.Export("testuser","deneme")
+	d = MySQL{}
+	d.Export("testuser", "deneme")
+
+}
+
+func TestMySQL_ExportWithError(t *testing.T) {
+	var d Dump
+	d = MySQL{}
+	err := d.Export("testuser", "deneme")
 	if err != nil {
-		t.Errorf("MySQL Export() Error: %s",err.Error())
+		t.Errorf("MySQL Export() Error: %s", err.Error())
 	}
 }
