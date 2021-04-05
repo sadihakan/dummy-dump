44e4617cf8f9e4b85b064706d9f92f34da00b04c
Onur CEVIK <onurcevik61@hotmail.com>
2021-04-05 15:04:48 +0300

    mysql binary path check fix

--- example/dummy_dump_example.go
+++ example/dummy_dump_example.go
@@ -6,15 +6,27 @@ import (
 )
 
 func main() {
-	dd, err := dummydump.New()
 
+	cfg := &config.Config{
+		Source:     config.MySQL,
+		Import:     false,
+		Export:     true,
+		User:       "testuser",
+		Password:   "123456",
+		Path:       "C:\\Program Files\\MySQL\\MySQL Server 8.0\\bin\\mysqldump.exe",
+		DB:         "deneme",
+		BinaryPath: "C:\\Program Files\\MySQL\\MySQL Server 8.0\\bin\\mysqldump.exe",
+		BackupName: "C:\\Users\\onur\\Desktop\\blah\\denefasdfme.bak",
+		Host:       "localhost",
+		Port:       3306,
+	}
+
+	dd, err := dummydump.New(cfg)
 	if err != nil {
 		panic(err)
 	}
 
-	dd.SetBinaryPath("/usr/bin/psql", config.PostgreSQL, false, true)
-
-	if _, err = dd.CheckPath().Run(); err != nil {
+	if _, err = dd.CheckPath().Export().Run(); err != nil {
 		panic(err)
 	}
 
