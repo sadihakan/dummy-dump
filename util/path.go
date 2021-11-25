eaafb64a1bf4eec621551fdf6777236f9167665e
akdilsiz <akdilsiz@tecpor.com>
2021-11-25 14:27:06 +0300

    Add command run directory

--- util/path.go
+++ util/path.go
@@ -1,6 +1,11 @@
 package util
 
-import "os"
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+	"runtime"
+)
 
 func PathExists(path string) bool {
 	_, err := os.Stat(path)
@@ -8,4 +13,20 @@ func PathExists(path string) bool {
 		return false
 	}
 	return !os.IsNotExist(err)
+}
+
+
+// HomeDir returns the base directory + backup-app
+// On Unix, including macOS, it returns the $HOME environment variable.
+// On Windows, it returns %USERPROFILE%/backup-app.
+// On Plan 9, it returns the $home environment variable.
+func HomeDir() string {
+	if runtime.GOOS == "windows" {
+		home := os.Getenv("HOMEDRIVE")
+		if home == "" {
+			home = os.Getenv("USERPROFILE")
+		}
+		return fmt.Sprintf("%s%s", home, string(filepath.Separator))
+	}
+	return os.Getenv("HOME")
 }
